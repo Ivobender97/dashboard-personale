3e75ee7507ef12259d72d0456487d4027eb067ab
Ivobender97 <ivansalgarello@gmail.com>
2025-05-25 13:33:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("collapsed");
-}
-
-function navigate(sectionId) {
-    document.querySelectorAll(".section").forEach(sec => {
-        sec.style.display = "none";
-    });
-    document.getElementById(sectionId).style.display = "block";
-}
+// Contenuto aggiornato del file script.js
