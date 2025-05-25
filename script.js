d0ed8bfea70d0f2588ef73822264c8e7c82257c4
Ivobender97 <ivansalgarello@gmail.com>
2025-05-25 09:11:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -18,9 +18,5 @@ function navigate(section) {
 
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
-    if (sidebar.style.display === 'none') {
-        sidebar.style.display = 'block';
-    } else {
-        sidebar.style.display = 'none';
-    }
+    sidebar.style.display = sidebar.style.display === 'none' ? 'block' : 'none';
 }
