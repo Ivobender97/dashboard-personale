4115cd4772be44d74f3eb76a4311012f623c5fb4
Ivobender97 <ivansalgarello@gmail.com>
2025-05-25 16:43:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function toggleSidebar() {
     sidebar.classList.toggle('collapsed');
     main.classList.toggle('collapsed');
 }
+
+function showSection(id) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+}
+
+// Tema scuro
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('darkToggle').addEventListener('change', function() {
+        document.body.classList.toggle('dark-mode', this.checked);
+    });
+});
